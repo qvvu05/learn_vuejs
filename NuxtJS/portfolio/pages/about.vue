1829337e611b2bd461ccf9da0a52ef132207ab9f
Quyen V. Vu <qvvu05@gmail.com>
2018-04-11 19:32:50 +0700

    add new page

---
+++ NuxtJS/portfolio/pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-medium">
+      <div class="container has-text-centered">
+          <h1 class="title">About Me</h1>
+          <p>My name is Quyen V. Vu,</p>
+      </div>
+  </section>
+</template>
---
+++ NuxtJS/portfolio/pages/contact.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="section is-medium">
+      <div class="container has-text-centered">
+          <h1 class="title">Contact</h1>
+          <p>
+              hello, what the fuck is this?
+              <a href="#">qvvu05@gmail</a>
+          </p>
+          
+      </div>
+  </section>
+</template>
